fb2661995d3f83e995aea6058a97d34feb112b8c
Abdul Aziz <abdulaziz.agie@gmail.com>
2025-08-20 09:53:59 +0500

    update

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -28,10 +28,10 @@ sudo usermod -aG docker jenkins
 
 newgrp docker
 
-sudo apt install awscli -y
-
-sudo usermod -a -G docker jenkins
-
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
 
 ## AWS configuration & restarts jenkins
 
